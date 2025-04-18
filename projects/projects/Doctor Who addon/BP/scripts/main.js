d7b5aec45b18fee16d6cd48534c27ae45912853d
Mr-Mycotic <Perilousbasher3@gmail.com>
2025-04-18 01:29:49 +0100

    Harriet pushed this for u <3
    UwU

--- projects/projects/Doctor Who addon/BP/scripts/main.js
+++ projects/projects/Doctor Who addon/BP/scripts/main.js
@@ -9,13 +9,34 @@ world.beforeEvents.worldInitialize.subscribe(
 )
 
 world.afterEvents.entityHurt.subscribe((event) => {
-    const { hurtEnt, damageSource } = event;
+    const { hurtEntity, damageSource } = event;
 
     if (damageSource.cause == "entityAttack" && damageSource.damagingEntity) {
         let attackingEnt = damageSource.damagingEntity;
 
         if (attackingEnt.typeId == "who_craft:weeping_angel") {
-            console.warn("hurt by weeping angel")
+            let xValue = Math.floor(Math.random() * 100);
+            let zValue = Math.floor(Math.random() * 100);
+
+            const availableDimensions = [];
+
+            availableDimensions[0] = "minecraft:overworld";
+            availableDimensions[1] = "minecraft:nether";
+            //availableDimensions[2] = "who_craft:classic";
+            //availableDimensions[3] = "who_craft:cave_game";
+
+            let dimensionValue = Math.floor(Math.random() * availableDimensions.length);
+
+            hurtEntity.teleport(
+                {
+                    x: hurtEntity.location.x + xValue,
+                    y: hurtEntity.location.y,
+                    z: hurtEntity.location.z + zValue
+                },
+                {
+                    checkForBlocks: true,
+                    dimension: world.getDimension(availableDimensions[dimensionValue])
+                });
         }
     }
 })
